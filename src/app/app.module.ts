380416a8ff1bc043e3b235c192b502453b7c062e
singh-preet <50730822+singh-preet@users.noreply.github.com>
2019-05-25 15:00:30 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
